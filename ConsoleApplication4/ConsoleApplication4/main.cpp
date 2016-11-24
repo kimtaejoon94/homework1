0edf7eab8374af3fecb5dd3266c624bf6b67ffdd
kimtaejoon94 <kimtaejoon@daum.net>
2016-11-24 16:45:02 +0900

    Add files via upload
    2016_11_24 pratice

--- ConsoleApplication4/ConsoleApplication4/main.cpp
+++ ConsoleApplication4/ConsoleApplication4/main.cpp
@@ -1,41 +1,51 @@
-#include <iostream>
-#include <process.h>
-#include "Game.h"
-#include "KeyEvent.h"
+#pragma once
+#include "Frame.h"
+#include <string>
+#include "ExArray.h"
 
-
-Game* game = new Game();
-unsigned int __stdcall mythread(void*)
+void main()
 {
-	int i = 0;
-	while (1) {
-		game->move();
-		Sleep(500);
-	}
-	return 0;
-}
-unsigned int __stdcall keyEvent(void*) {
-	KeyEvent k;
-	int i;
-	while (1) {
-		i = k.getkey();
-		if (i != -1) game->setDirectiong(i);
-	}
+	ExArray<int>arr1(3);
+	ExArray<char> arr2(4);
+	ExArray<double> arr3(5);
 
-	return 0;
+	arr1.addData(2);
+	arr2.addData('A');
+	arr3.addData(32.12);
+
+	arr1.prinData();
+	arr2.prinData();
+	arr3.prinData();
+
+	int num1 = 10, num2 = 40;
+	cout << "before" << num1 << "," << num2 << endl;
+	mySwap<int>(num1, num2);
+	cout << "after" << num1 << "," << num2 << endl;
+
+	double num1d = 121.11, num2d = 10.5;
+	cout << "before" << num1d << "," << num2d << endl;
+	mySwap<int>(num1, num2);
+	cout << "after" << num1d << "," << num2d << endl;
+
+	ExArray<int> arr4(5);
+	arr4.addData(10);
+
+	mySwap<ExArray<int >> (arr1, arr4);
+	arr1.prinData();
+	arr4.prinData();
 }
 
-int main()
+/*void main()
 {
-	HANDLE game1=0, game2=0;
-	game1 = (HANDLE)_beginthreadex(0, 0, &mythread, (void*)0, 0, 0);
-	game1 = (HANDLE)_beginthreadex(0, 0, &keyEvent, (void*)0, 0, 0);
+	try {
+		Frame *f1 = new Frame;
+		Frame *f2 = new Frame[10];
 
-	WaitForSingleObject(game1, INFINITE);
-	WaitForSingleObject(game2, INFINITE);
-
-	CloseHandle(game1);
-	CloseHandle(game2);
-
-	return 0;
-}
+		delete f1;
+		delete[] f2;
+	}
+	catch (string msg)
+	{
+		cout << "Error msg:" << msg << endl;
+	}
+}*/
